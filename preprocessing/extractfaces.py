487ac361ba9040194335e9be9a62853092f88ecf
Anahita Aria <78658893+anahita-aria@users.noreply.github.com>
2023-08-24 20:43:40 +0330

    Update extractfaces.py
    fs

--- preprocessing/extractfaces.py
+++ preprocessing/extractfaces.py
@@ -19,7 +19,7 @@ from blazeface import BlazeFace
 
 # code used to extract DFDC dataset
 
-dir_path = "./dfdc_data/dfdc_train_part_3" 
+dir_path = "dfdc_data" 
 train_path = "dfdc_data/training_face_"
 validation_path = "dfdc_data/validation_face_"
 test_path = "dfdc_data/test_face_"
